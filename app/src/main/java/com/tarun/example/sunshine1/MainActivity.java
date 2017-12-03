4140c8da87d1c237015bace0d0711de3a25c1992
TarunISCO <201552063@iiitvadodara.ac.in>
2017-12-04 00:51:58 +0530

    end of lesson 2

--- app/src/main/java/com/tarun/example/sunshine1/MainActivity.java
+++ app/src/main/java/com/tarun/example/sunshine1/MainActivity.java
@@ -1,28 +1,9 @@
 package com.tarun.example.sunshine1;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -57,51 +38,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        ArrayAdapter<String> mForecastAdapter;
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            String[] data = {
-                    "Mon 6/23 - Sunny - 31/17",
-                    "Tue 6/24 - Foggy - 21/8",
-                    "Wed 6/25 - Cloudy - 22/17",
-                    "Thurs 6/26 - Rainy - 18/11",
-                    "Fri 6/27 - Foggy - 21/10",
-                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                    "Sun 6/29 - Sunny - 20/7"
-            };
-
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
-
-            // Set adapter to dummy data
-            mForecastAdapter = new ArrayAdapter<String>(
-                    getActivity(),                      // Current Context (this activity)
-                    R.layout.list_item_forecast,        // name of layout id
-                    R.id.list_item_forecast_textview,   // id of textview to populate
-                    weekForecast                        // input data to generate list
-            );
-
-
-
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            // Get reference to the list view and bind adapter
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(mForecastAdapter);
-
-            
-            return rootView;
-        }
     }
 }
